aae582a53df0a6f45117528b608be1d285605a5e
davicami <100150296+davicami@users.noreply.github.com>
2022-11-30 15:28:27 +0100

    Created home page and header-products component to show products

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+
+@Component({
+  selector: "app-products-header",
+  templateUrl: "products-header.component.html",
+  styles: [],
+})
+export class ProductsHeaderComponent implements OnInit {
+  @Output() columnsCountChange = new EventEmitter<number>();
+  sort = "desc";
+  itemsShowCount = 12;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+
+  onItemsUpdated(newItemsCount: number): void {
+    this.itemsShowCount = newItemsCount;
+  }
+
+  onColumnsUpdated(newColsNum: number): void {
+    this.columnsCountChange.emit(newColsNum);
+  }
+}
